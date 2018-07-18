22e0147916583551ecc655c6864233baaca3631d
renosz <32728131+renosz@users.noreply.github.com>
2018-07-18 18:50:43 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const { hostname, platform } = require('os');
 
-const whichPlatform = platform() === 'darwin' ? 'MAC' : 'win32';
+const whichPlatform = platform() === 'KCML' ? 'CHROME' : 'win64';
 
 const config = {
   LINE_DOMAIN_INDO: 'gd2i.line.naver.jp',
@@ -36,7 +36,7 @@ const config = {
   LINE_SESSION_NAVER_URL: '/authct/v1/keys/naver',
   LINE_POST_CONTENT_URL: 'https://os.line.naver.jp/talk/m/upload.nhn',
   LINE_POST_CONTENT_URL_2ND: 'https://obs-sg.line-apps.com/talk/m/upload.nhn',
-  X_LINE_APP: 'IOSIPAD 7.14.0 iPhone OS 10.12.0',
+  X_LINE_APP: 'CHROMEOS 2.1.5 Chrome_OS 1',
   //X_LINE_APP: 'CHROMEOS 1.4.13  Chrome_OS 1',
   ip: '127.0.0.1',
   version: '0.0.2',
@@ -45,7 +45,7 @@ const config = {
   platform: whichPlatform,
   EMAIL_REGEX: /[^@]+@[^@]+\.[^@]+/,
   Headers: {
-    'User-Agent':'Line/7.18.1'
+    'User-Agent':'Line/2.1.5'
   }
 };
 
